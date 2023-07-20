229c1a0b79964708b02c6b2b843d56e2159bd6a7
Kynson Szetau <46522440+Kynson@users.noreply.github.com>
2023-07-20 13:42:48 +0800

    Update time.py

--- src/spyc_iot/time.py
+++ src/spyc_iot/time.py
@@ -1,8 +1,9 @@
 import time
 
-def get_time_string(separator = ':'):
+def get_time_string():
     current_time = time.gmtime()
     hour = f'{current_time[3] + 8:02}'
     minute = f'{current_time[4]:02}'
+    second = f'{current_time[5]:02}'
     
-    return f'{hour}{separator}{minute}'
+    return f'{hour} {minute} {second}'
